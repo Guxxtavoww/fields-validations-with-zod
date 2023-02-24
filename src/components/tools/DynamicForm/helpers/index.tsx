3c0e37d3eba368cef596080b3ee1d03e6527f4a1
Gustavo <gustavoaugustocar@hotmail.com>
2023-02-24 08:36:42 -0300

    code progress

--- src/components/tools/DynamicForm/helpers/index.tsx
+++ src/components/tools/DynamicForm/helpers/index.tsx
@@ -2,15 +2,19 @@ import { lazy } from 'react';
 
 import { iInputProps } from '../types';
 
-export function handleRenderInputs<T>(props: iInputProps<T>, index: any): JSX.Element {
+export function handleRenderInputs<T>(
+  props: iInputProps<T>,
+  index: any
+): JSX.Element {
   const componentMapping = {
     text: lazy(() => import('../../Input')),
     number: lazy(() => import('../../Input')),
     email: lazy(() => import('../../Input')),
     password: lazy(() => import('../../Input')),
-  }
+  };
 
-  const Component = componentMapping[props.type as keyof typeof componentMapping];
+  const Component =
+    componentMapping[props.type as keyof typeof componentMapping];
 
-  return <Component {...props} name={props.input_name as string} key={index} />
-};
+  return <Component {...props} name={props.input_name as string} key={index} />;
+}
