6cc4a0c722b4e13769421ac17b7bcac99777fba7
Gustavo <gustavoaugustocar@hotmail.com>
2023-02-24 14:40:02 -0300

    code progress

--- src/utils/formFunctions.ts
+++ src/utils/formFunctions.ts
@@ -1,7 +1,8 @@
+import { RefObject } from 'react';
 import { FormHandles } from '@unform/core';
 
 type fieldNameType<T> = keyof T;
-type unformRef = React.RefObject<FormHandles>;
+type unformRef = RefObject<FormHandles>;
 
 export const getFieldValue = <T>(
   ref: unformRef,
@@ -10,13 +11,13 @@ export const getFieldValue = <T>(
   return ref.current?.getFieldValue(fieldName.toString()) as string | boolean;
 };
 
-interface IParams<T> {
+interface iParams<T> {
   fieldName: fieldNameType<T>;
   value: any;
   isError?: boolean;
 }
 
-export const setFieldValue = <T>(ref: unformRef, params: IParams<T>) => {
+export const setFieldValue = <T>(ref: unformRef, params: iParams<T>) => {
   const { fieldName, value, isError } = params;
 
   const field = fieldName.toString();
